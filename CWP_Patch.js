5147f3d9c37d200f2236cc157c0ba7de3e013c08
CallCateIn58 <jonm58@outlook.com>
2023-10-19 18:48:42 +0800

    Add files via upload

---
+++ CWP_Patch.js
@@ -0,0 +1,10 @@
+//Chinese Web Patch for PaleMoon
+//Repair Chinese Web  for PaleMoon(在PaleMoon中修复中文网页)
+//By CallCaeIn58(Jonm58) - 2023-?
+pref("devtools.gcli.jquerySrc", "https://cdnjs.loli.net/ajax/libs/jquery/2.2.1/jquery.min.js");
+pref("devtools.gcli.lodashSrc", "https://cdnjs.loli.net/ajax/libs/lodash.js/4.6.1/lodash.min.js");
+pref("devtools.gcli.underscoreSrc", "https://cdnjs.loli.net/ajax/libs/underscore.js/1.8.3/underscore-min.js");
+pref("general.useragent.override.bilibili.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.hdslb.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.geetest.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.acfun.cn", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
