c0e9bec0649fa960f13e6592320b9b04bf995261
CallCateIn58 <jonm58@outlook.com>
2023-12-04 04:59:40 +0000

    Update CWP_Patch.js

--- CWP_Patch.js
+++ CWP_Patch.js
@@ -5,7 +5,7 @@
 pref("devtools.gcli.jquerySrc"， "https://cdnjs.loli.net/ajax/libs/jquery/2.2.1/jquery.min.js");
 pref("devtools.gcli.lodashSrc"， "https://cdnjs.loli.net/ajax/libs/lodash.js/4.6.1/lodash.min.js");
 pref("devtools.gcli.underscoreSrc"， "https://cdnjs.loli.net/ajax/libs/underscore.js/1.8.3/underscore-min.js");
-pref("general.useragent.override.bilibili.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
-pref("general.useragent.override.hdslb.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
-pref("general.useragent.override.geetest.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
-pref("general.useragent.override.acfun.cn"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.bilibili.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6160.0 Safari/537.36");
+pref("general.useragent.override.hdslb.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6160.0 Safari/537.36");
+pref("general.useragent.override.geetest.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6160.0 Safari/537.36");
+pref("general.useragent.override.acfun.cn"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6160.0 Safari/537.36");
