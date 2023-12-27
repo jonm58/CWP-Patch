23b76b8fec7a8c21bb1cbd9e52d52ba44103399c
CallCateIn58 <jonm58@outlook.com>
2023-12-27 12:31:03 +0000

    Update and rename CWP_Patch.js to PM_CWP_Patch.js

---
+++ PM_CWP_Patch.js
@@ -0,0 +1,15 @@
+//Chinese Web Patch for PaleMoon
+//Repair Chinese Web  for PaleMoon(在PaleMoon中修复中文网页)
+//By CallCaeIn58(Jonm58) - 2023-?
+
+pref("devtools.gcli.jquerySrc"， "https://cdnjs.loli.net/ajax/libs/jquery/2.2.1/jquery.min.js");
+pref("devtools.gcli.lodashSrc"， "https://cdnjs.loli.net/ajax/libs/lodash.js/4.6.1/lodash.min.js");
+pref("devtools.gcli.underscoreSrc"， "https://cdnjs.loli.net/ajax/libs/underscore.js/1.8.3/underscore-min.js");
+pref("general.useragent.override.bilibili.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.hdslb.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.geetest.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.acfun.cn"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.youtube.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.baidu.com"， "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6066.0 Safari/537.36");
+pref("general.useragent.override.zhihu.com"， "Mozilla/5.0 (Linux; Android 12; FOA-AL00 Build/HUAWEIFOA-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.88 Mobile Safari/537.36");
+pref("general.useragent.override.discord.com"， "Mozilla/5.0 (Linux; Android 12; HarmonyOS; FOA-AL00; HMSCore 6.12.4.312; GMSCore 22.50.14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 HuaweiBrowser/14.0.5.301 Mobile Safari/537.36");
